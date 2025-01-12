e2970ecf9efcd525cbac2d15851ea12b2b99d873
Rumba19 <brumba30721@ucumberlands.edu>
2025-01-11 18:46:08 -0800

    Sorting array descending

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,23 @@
-print("test configuration")
+#Insertion Sort Algorithm in decreasing order
+
+def insertion_sort_descending(arr):
+    length = len(arr)
+    for i in range(1, length):
+        key = arr[i]   
+        j = i - 1   
+        # Move elements less than key one position ahead
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+ 
+array = [1, 10, 53, 4, 7]
+print("Original Array 1:", array)
+insertion_sort_descending(array)
+print("Sorted Array in Decreasing Order: 1", array)
+ 
+array = [1, 3, 5, 4, 7]
+print("Original Array 2:", array)
+insertion_sort_descending(array)
+print("Sorted Array in Decreasing Order 2:", array)
